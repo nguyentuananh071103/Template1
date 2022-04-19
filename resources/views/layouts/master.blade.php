278655a75851a65306b8aa94cfb2a9c15a38d4d7
nguyentuananh071103 <nguyenngocbao13579@gmail.com>
2022-04-19 10:06:46 +0700

    tap cat giao dien chia thu muc

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Responsive Bootstrap4 Shop Template, Created by Imran Hossain from https://imransdesign.com/">
+
+    <!-- title -->
+    <title>Fruitkha</title>
+
+    <!-- favicon -->
+    <link rel="shortcut icon" type="image/png" href="assets/img/favicon.png">
+    <!-- google font -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="{{asset('assets/css/all.min.css')}}">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap.min.css')}}">
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="{{asset('assets/css/owl.carousel.css')}}">
+    <!-- magnific popup -->
+    <link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
+    <!-- animate css -->
+    <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
+    <!-- mean menu css -->
+    <link rel="stylesheet" href="{{asset('assets/css/meanmenu.min.css')}}">
+    <!-- main style -->
+    <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
+    <!-- responsive -->
+    <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}">
+
+</head>
+<body>
+
+<!--PreLoader-->
+<div class="loader">
+    <div class="loader-inner">
+        <div class="circle"></div>
+    </div>
+</div>
+<!--PreLoader Ends-->
+
+<!-- header -->
+@include('partial.header')
+<!-- end header -->
+
+<!-- search area -->
+<div class="search-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <span class="close-btn"><i class="fas fa-window-close"></i></span>
+                <div class="search-bar">
+                    <div class="search-bar-tablecell">
+                        <h3>Search For:</h3>
+                        <input type="text" placeholder="Keywords">
+                        <button type="submit">Search <i class="fas fa-search"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end search area -->
+
+<!-- hero area -->
+<div class="hero-area hero-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9 offset-lg-2 text-center">
+                <div class="hero-text">
+                    <div class="hero-text-tablecell">
+                        <p class="subtitle">Fresh & Organic</p>
+                        <h1>Delicious Seasonal Fruits</h1>
+                        <div class="hero-btns">
+                            <a href="shop.html" class="boxed-btn">Fruit Collection</a>
+                            <a href="contact.html" class="bordered-btn">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end hero area -->
+
+<!-- features list section -->
+@include('pages.features')
+<!-- end features list section -->
+
+<!-- product section -->
+@include('pages.product_section')
+<!-- end product section -->
+
+<!-- cart banner section -->
+@include('pages.cart_banner')
+<!-- end cart banner section -->
+
+<!-- testimonail-section -->
+@include('pages.testimonail')
+<!-- end testimonail-section -->
+
+<!-- advertisement section -->
+@include('pages.advertisement')
+<!-- end advertisement section -->
+
+<!-- shop banner -->
+@include('pages.shop_banner')
+<!-- end shop banner -->
+
+<!-- latest news -->
+@include('pages.latest_news')
+<!-- end latest news -->
+
+<!-- logo carousel -->
+@include('pages.logo_carousel')
+<!-- end logo carousel -->
+
+<!-- footer -->
+@include('partial.footer')
+<!-- end footer -->
+@include('pages.copyrights')
+<!-- copyright -->
+<!-- end copyright -->
+
+<!-- jquery -->
+<script src="{{asset('assets/js/jquery-1.11.3.min.js')}}"></script>
+<!-- bootstrap -->
+<script src="{{asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+<!-- count down -->
+<script src="{{asset('assets/js/jquery.countdown.js')}}"></script>
+<!-- isotope -->
+<script src="{{asset('assets/js/jquery.isotope-3.0.6.min.js')}}"></script>
+<!-- waypoints -->
+<script src="{{asset('assets/js/waypoints.js')}}"></script>
+<!-- owl carousel -->
+<script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
+<!-- magnific popup -->
+<script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+<!-- mean menu -->
+<script src="{{asset('assets/js/jquery.meanmenu.min.js')}}"></script>
+<!-- sticker js -->
+<script src="{{asset('assets/js/sticker.js')}}"></script>
+<!-- main js -->
+<script src="{{asset('assets/js/main.js')}}"></script>
+
+</body>
+</html>
